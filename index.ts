e028acb82b2a01cbb3a0140b2546b067f9fbadcc
kulwinder007 <kulwindersingh915.ca@gmail.com>
2025-03-23 21:38:54 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 
-// Creating S3 bucket for website hosting
+// Create S3 bucket for website hosting
 const siteBucket = new aws.s3.Bucket("siteBucket", {
     website: {
         indexDocument: "index.html",
